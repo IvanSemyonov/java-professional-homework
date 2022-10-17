6d7d50ca9f7d19ab8f57d7f17d08f6880c14e549
semenovio <semyonov.o2001@gmail.com>
2022-10-17 22:11:36 +0300

    hw2 generics

--- hw04-generics/src/main/java/homework/CustomerService.java
+++ hw04-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,36 @@
 package homework;
 
 
-import java.util.Map;
+import java.util.*;
 
 public class CustomerService {
+
+    private final TreeMap<Customer, String> customerData = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
     public Map.Entry<Customer, String> getSmallest() {
-        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> customerDataEntry = customerData.firstEntry();
+        if (customerDataEntry == null) {
+            return null;
+        }
+        return cloneEntry(customerDataEntry);
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> customerDataEntry = customerData.higherEntry(customer);
+        if (customerDataEntry == null) {
+            return null;
+        }
+        return cloneEntry(customerDataEntry);
+    }
+
+    private Map.Entry<Customer, String> cloneEntry(Map.Entry<Customer, String> customerDataEntry) {
+        return Map.entry(customerDataEntry.getKey().clone(), customerDataEntry.getValue());
     }
 
     public void add(Customer customer, String data) {
-
+        customerData.put(customer.clone(), data);
     }
 }
