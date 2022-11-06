acff7ea0a6a513bed6ced908bf63d516429a5112
semenovio <semyonov.o2001@gmail.com>
2022-11-06 12:14:31 +0300

    hw10-byteCodes Work on bugs

--- hw10-byteCodes/src/main/java/ru/otus/proxy/LoggingInvocationHandler.java
+++ hw10-byteCodes/src/main/java/ru/otus/proxy/LoggingInvocationHandler.java
@@ -1,22 +1,41 @@
 package ru.otus.proxy;
 
 import ru.otus.annotations.Log;
+import ru.otus.reflection.ReflectionHelper;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class LoggingInvocationHandler<T> implements InvocationHandler {
     private final T myClass;
+    private final Set<Method> loggableMethods;
 
     LoggingInvocationHandler(T myClass) {
+        Method[] methods = myClass.getClass().getDeclaredMethods();
+        this.loggableMethods = getLoggableMethods(methods);
         this.myClass = myClass;
+    }
+
+    private Set<Method> getLoggableMethods(Method[] methods) {
+        Set<Method> annotatedLogMethods = new HashSet<>();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Log.class)) {
+                annotatedLogMethods.add(method);
+            }
+        }
+        return annotatedLogMethods;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        method = myClass.getClass().getMethod(method.getName(), method.getParameterTypes());
-        if (method.isAnnotationPresent(Log.class)) {
-            logMethod(method, args);
+        for (Method loggableMethod : loggableMethods) {
+            if (ReflectionHelper.equalMethods(loggableMethod, method)) {
+                logMethod(method, args);
+                break;
+            }
         }
 
         return method.invoke(myClass, args);
--- hw10-byteCodes/src/main/java/ru/otus/reflection/ReflectionHelper.java
+++ hw10-byteCodes/src/main/java/ru/otus/reflection/ReflectionHelper.java
@@ -1,5 +1,6 @@
 package ru.otus.reflection;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 
@@ -20,5 +21,25 @@ public class ReflectionHelper {
 
     public static Class<?>[] toClasses(Object[] args) {
         return Arrays.stream(args).map(Object::getClass).toArray(Class<?>[]::new);
+    }
+
+    public static boolean equalMethods(Method method1, Method method2){
+        return method1.getName().equals(method2.getName())
+                && method1.getReturnType().equals(method2.getReturnType())
+                && equalParamTypes(method1.getParameterTypes(), method2.getParameterTypes());
+    }
+
+    public static boolean equalParamTypes(Class<?>[] params1, Class<?>[] params2) {
+        if (params1.length == params2.length) {
+            for(int i = 0; i < params1.length; ++i) {
+                if (params1[i] != params2[i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        } else {
+            return false;
+        }
     }
 }
