ac480a0e17da38269c473ca4fd42dd4f7d386b72
semenovio <semyonov.o2001@gmail.com>
2023-01-04 19:50:19 +0300

    hw15 without changes

--- hw04-generics/src/main/java/homework/CustomerService.java
+++ hw04-generics/src/main/java/homework/CustomerService.java
@@ -7,9 +7,6 @@ public class CustomerService {
 
     private final TreeMap<Customer, String> customerData = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
-    //todo: 3. надо реализовать методы этого класса
-    //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
-
     public Map.Entry<Customer, String> getSmallest() {
         Map.Entry<Customer, String> customerDataEntry = customerData.firstEntry();
         if (customerDataEntry == null) {
