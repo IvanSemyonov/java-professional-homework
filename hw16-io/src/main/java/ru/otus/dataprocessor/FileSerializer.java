415683cd1b922b068a907b13a3741fce14b19cc5
semenovio <semyonov.o2001@gmail.com>
2023-01-06 14:20:46 +0300

    hw16 JSON handler

--- hw16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,26 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
+    private final String fileURI;
 
     public FileSerializer(String fileName) {
+        this.fileURI = fileName;
     }
 
     @Override
-    public void serialize(Map<String, Double> data) {
+    public void serialize(Map<String, Double> data) throws IOException {
         //формирует результирующий json и сохраняет его в файл
+        Gson gson = new Gson();
+        try(OutputStreamWriter writer = new OutputStreamWriter(
+                new FileOutputStream(fileURI))) {
+            gson.toJson(data, writer);
+        }
     }
 }
--- hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,28 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import ru.otus.model.Measurement;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
+    private final String file;
+
     public ResourcesFileLoader(String fileName) {
+        this.file = ClassLoader.getSystemResource(fileName).getFile();
     }
 
     @Override
-    public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+    public List<Measurement> load() throws IOException {
+        Gson gson = new Gson();
+        try(InputStreamReader reader = new InputStreamReader(new FileInputStream(file))) {
+            return gson.fromJson(reader, new TypeToken<List<Measurement>>(){}.getType());
+        }
     }
 }
